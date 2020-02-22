3861b0e99c28c840b54a4c85afd84639d7529d34
ThePieMonster <zacbraasch@gmail.com>
2020-02-22 13:35:55 -0800

    Update WaniKani-PlayAudioViaKeyPress.user.js
    - Added support for 2nd pronunciation group (4 keys can be mapped instead of 2 now)
    - Fixed bug that would carry over audio from a prior reading

--- WaniKani-PlayAudioViaKeyPress.user.js
+++ WaniKani-PlayAudioViaKeyPress.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           WaniKani Play Audio Via Key Press
 // @namespace      https://github.com/ThePieMonster
-// @version        1.0.0
+// @version        1.1.0
 // @description    Play the audio file for the active review item via a key press
 // @author         ThePieMonster
 // @include        https://www.wanikani.com/review/session
@@ -15,17 +15,24 @@
 
 // ----------- User Setting -----------
 
-var audioKeyPressKyoko = 49; // 1
-var audioKeyPressKenichi = 50; // 2
+var audioKeyPressKyoko1 = 49; // 1
+var audioKeyPressKenichi1 = 50; // 2
+var audioKeyPressKyoko2 = 51; // 3
+var audioKeyPressKenichi2 = 52; // 4
 
 // --------------- Code ---------------
 
-console.log("WK Audio Play: Starting Script with Kyoto key binding '" + audioKeyPressKyoko + "' and Kenichi key binding '" + audioKeyPressKenichi + "'.");
+console.log("WK Audio Play: Starting Script with Kyoko 1 key binding '" + audioKeyPressKyoko1 + "' and Kenichi 1 key binding '" + audioKeyPressKenichi1 +
+            "' and Kyoko 2 key binding '" + audioKeyPressKyoko2 + "' and Kenichi 2 key binding '" + audioKeyPressKenichi2 + "'.");
 
-var kyoko;
-var kenichi;
-var audioKyoko;
-var audioKenichi;
+var kyoko1;
+var kenichi1;
+var kyoko2;
+var kenichi2;
+var audioKyoko1;
+var audioKenichi1;
+var audioKyoko2;
+var audioKenichi2;
 
 whenLoadingIndicatorIsHidden(function () {
     getAudio();
@@ -59,17 +66,15 @@ function getOptionItemInfo() {
         } else {
             console.log("WK Audio Play: Answer submitted");
             try {
-                // Get Kyoko Audio String
-                window.kyoko = document.querySelector("#item-info-reading > div > div > div > ul > li:nth-child(1) > span.audio-player > audio > source:nth-child(1)"); // audio/mpeg
-                // window.kyoko = document.querySelector("#item-info-reading > div > div > div > ul > li:nth-child(1) > span.audio-player > audio > source:nth-child(2)"); // audio/ogg
-
-                // Get Kenichi Audio String
-                window.kenichi = document.querySelector("#item-info-reading > div > div > div > ul > li:nth-child(2) > span.audio-player > audio > source:nth-child(1)"); // audio/mpeg
-                // window.kenichi = document.querySelector("#item-info-reading > div > div > div > ul > li:nth-child(2) > span.audio-player > audio > source:nth-child(2)"); // audio/ogg
-
-                // Setup Audio String
-                window.audioKyoko = new Audio(window.kyoko.src);
-                window.audioKenichi = new Audio(window.kenichi.src);
+                // Get Audio Strings
+                window.kyoko1 = null;
+                window.kenichi1 = null;
+                window.kyoko2 = null;
+                window.kenichi2 = null;
+                window.kyoko1 = $('.pronunciation-group audio')[0];
+                window.kenichi1 = $('.pronunciation-group audio')[1];
+                window.kyoko2 = $('.pronunciation-group audio')[2];
+                window.kenichi2 = $('.pronunciation-group audio')[3];
             }
             catch(err) {
                 console.log(err.message);
@@ -80,13 +85,21 @@ function getOptionItemInfo() {
     // Listen For Key Press
     document.addEventListener('keydown', function(event){
         if (!event.repeat) {
-            if (event.keyCode === audioKeyPressKyoko) {
-                console.log("WK Audio Play: Playing Audio Kyoko");
-                window.audioKyoko.play();
+            if (event.keyCode === audioKeyPressKyoko1) {
+                console.log("WK Audio Play: Playing Audio Kyoko 1");
+                try {window.kyoko1.play();} catch(err) {console.log(err.message);}
             }
-            if (event.keyCode === audioKeyPressKenichi) {
-                console.log("WK Audio Play: Playing Audio Kenichi");
-                window.audioKenichi.play();
+            if (event.keyCode === audioKeyPressKenichi1) {
+                console.log("WK Audio Play: Playing Audio Kenichi 1");
+                try {window.kenichi1.play();} catch(err) {console.log(err.message);}
+            }
+            if (event.keyCode === audioKeyPressKyoko2) {
+                console.log("WK Audio Play: Playing Audio Kyoko 2");
+                try {window.kyoko2.play();} catch(err) {console.log(err.message);}
+            }
+            if (event.keyCode === audioKeyPressKenichi2) {
+                console.log("WK Audio Play: Playing Audio Kenichi 2");
+                try {window.kenichi2.play();} catch(err) {console.log(err.message);}
             }
         }
     });
